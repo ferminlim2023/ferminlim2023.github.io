60c2466d1978f3af495829d82f42681dec52df4c
ferminlim2023 <fermin.lim.2023@scis.smu.edu.sg>
2024-08-28 15:02:57 +0800

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".column img");
-    images.forEach((img, index) => {
-        setTimeout(() => {
-            img.style.opacity = 1;
-            img.style.transform = "scale(1)";
-        }, index * 200); // Stagger the animations
-    });
-});
